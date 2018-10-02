abb601f92f078d97757c87b9a264bf9198b4d2e0
WiseLord <wiselord1983@gmail.com>
2018-10-02 09:20:30 +0300

    Some improvements for 480x320 layout

--- display/gc480x320/gc480x320.c
+++ display/gc480x320/gc480x320.c
@@ -23,7 +23,7 @@ static void displayTm(RTC_type *rtc, uint8_t tm)
 static void drawShowBar(int16_t value, int16_t min, int16_t max)
 {
     static const int16_t sc = 80; // Scale count
-    static const uint8_t sw = 2; // Scale width
+    static const uint8_t sw = 3; // Scale width
 
     if (min + max) { // Non-symmectic scale => rescale to 0..sl
         value = sc * (value - min) / (max - min);
@@ -47,19 +47,23 @@ static void drawShowBar(int16_t value, int16_t min, int16_t max)
             }
         }
 
-        glcdDrawRect(i * (glcd->canvas->width / sc) + 1, 84, sw, 28, color);
-        glcdDrawRect(i * (glcd->canvas->width / sc) + 1, 98, sw, 2, LCD_COLOR_WHITE);
-        glcdDrawRect(i * (glcd->canvas->width / sc) + 1, 100, sw, 28, color);
+        glcdDrawRect(i * (glcd->canvas->width / sc) + 1, 100, sw, 14, color);
+        glcdDrawRect(i * (glcd->canvas->width / sc) + 1, 114, sw, 2, LCD_COLOR_WHITE);
+        glcdDrawRect(i * (glcd->canvas->width / sc) + 1, 116, sw, 14, color);
     }
 }
 
-static void drawSpCol(uint16_t xbase, uint16_t ybase, uint8_t width, uint16_t value, uint16_t max)
+static void drawSpCol(uint16_t xbase, uint16_t ybase, uint8_t width, uint8_t value, uint8_t max, uint8_t peak)
 {
     if (value > max)
         value = max;
 
+    if (peak > max - 1)
+        peak = max - 1;
+
     glcdDrawRect(xbase, ybase - value, width, value, LCD_COLOR_AQUA);
     glcdDrawRect(xbase, ybase - max, width, max - value, LCD_COLOR_BLACK);
+    glcdDrawRect(xbase, ybase - peak - 1, width, 1, LCD_COLOR_YELLOW);
 }
 
 static void showTime(RTC_type *rtc, char *wday)
@@ -90,13 +94,13 @@ static void showTime(RTC_type *rtc, char *wday)
     glcdWriteChar(LETTER_SPACE_CHAR);
     displayTm(rtc, RTC_YEAR);
 
-    glcdSetXY(240, 220);
+    glcdSetXY(240, 224);
     glcdSetFont(&fontterminusmod96);
     glcdSetFontColor(LCD_COLOR_AQUA);
 
     static char *wdayOld = 0;
     if (wday != wdayOld) {
-        glcdDrawRect(0, 220, 480, 96, glcd->canvas->color);
+        glcdDrawRect(0, 224, 480, 9, glcd->canvas->color);
     }
 
     glcdSetFontAlign(FONT_ALIGN_CENTER);
@@ -124,27 +128,32 @@ static void showParam(DispParam *dp)
 static void showSpectrum(SpectrumData *spData)
 {
     uint8_t *buf;
+    uint8_t *peak;
 
     buf = spData[SP_CHAN_LEFT].show;
+    peak = spData[SP_CHAN_LEFT].peak;
     for (uint16_t x = 0; x < (glcd->canvas->width + 1) / 4; x++) {
         uint16_t xbase = x * 4;
         uint16_t ybase = 160;
         uint16_t width = 2;
-        uint16_t value = buf[x];
-        uint16_t max = 159;
+        uint8_t value = buf[x];
+        uint8_t pValue = peak[x];
+        uint8_t max = 159;
 
-        drawSpCol(xbase, ybase, width, value + 1, max);
+        drawSpCol(xbase, ybase, width, value + 1, max, pValue);
     }
 
     buf = spData[SP_CHAN_RIGHT].show;
+    peak = spData[SP_CHAN_RIGHT].peak;
     for (uint16_t x = 0; x < (glcd->canvas->width + 1) / 4; x++) {
         uint16_t xbase = x * 4;
         uint16_t ybase = 320;
         uint16_t width = 2;
-        uint16_t value = buf[x];
-        uint16_t max = 159;
+        uint8_t value = buf[x];
+        uint8_t pValue = peak[x];
+        uint8_t max = 159;
 
-        drawSpCol(xbase, ybase, width, value + 1, max);
+        drawSpCol(xbase, ybase, width, value + 1, max, pValue);
     }
 }
 
