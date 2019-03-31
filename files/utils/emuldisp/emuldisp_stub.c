ed79bd876c211c1495e54d349c395366dd1d5171
WiseLord <wiselord1983@gmail.com>
2019-03-31 11:25:22 +0300

    Emuldisp: quadratic random

--- files/utils/emuldisp/emuldisp_stub.c
+++ files/utils/emuldisp/emuldisp_stub.c
@@ -119,8 +119,8 @@ Spectrum *spGet(void)
 void spGetADC(uint8_t *dataL, uint8_t *dataR)
 {
     for (uint8_t i = 0; i < SPECTRUM_SIZE; i++) {
-        dataL[i] = rand() & 0xFF;
-        dataR[i] = rand() & 0xFF;
+        dataL[i] = (rand() & 0xFF) * (rand() & 0xFF) / 256;
+        dataR[i] = (rand() & 0xFF) * (rand() & 0xFF) / 256;
     }
 }
 
