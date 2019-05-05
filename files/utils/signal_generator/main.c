f070b0f4730d34a27530eb58d14bf18b8eaa9efa
WiseLord <wiselord1983@gmail.com>
2019-05-06 01:28:43 +0300

    Improved signal generator

--- files/utils/signal_generator/main.c
+++ files/utils/signal_generator/main.c
@@ -13,29 +13,27 @@
 #define N_DB        256
 #define DB_MAX      65535
 
-int16_t input[FFT_SIZE];
-
-int main()
+void print_sinus_table(void)
 {
+    printf("Sinus:\n");
+
     double t = 0;
 
-    // Sinus
-    for (int i = 0; i < FFT_SIZE; i++) {
+    for (int i = 0; i < FFT_SIZE / 4 + 1; i++) {
         t = (double)i / SAMPLE_FREQ;
 
         printf("%6d, ", (int16_t)(SIGNAL_SCALE * sin(2 * M_PI * SIGNAL_FREQ * t)));
         if (i % 8 == 7)
             printf("\n");
     }
-    printf("\n");
+    printf("\n\n");
+}
 
-    // Hamming window
-    double a0 = 0.53836;
-    double a1 = 0.46164;
-//    double a0 = 0.5; // Hann window
-//    double a1 = 0.5; // Hann window
+void print_hamming_table(double a0, double a1)
+{
+    printf("Hamming window:\n");
 
-    for (int i = 0; i < FFT_SIZE; i++) {
+    for (int i = 0; i < FFT_SIZE / 2; i++) {
         double w;
         uint16_t hw;
 
@@ -47,32 +45,15 @@ int main()
             printf("\n");
     }
     printf("\n");
+}
 
+void print_db_table(double power, int N)
+{
+    printf("Db table:\n");
     for (int i = 0; i < N_DB; i++) {
         double l;
 
         l = exp((double)i / (N_DB - 1) * log(DB_MAX));
-
-//        double power = 1;
-//        double N = 0;
-
-//        double power = 0.9;
-//        double N = 2;
-
-//        double power = 0.8;
-//        double N = 8;
-
-//        double power = 0.7;
-//        double N = 27;
-
-//        double power = 0.6;
-//        double N = 84;
-
-//        double power = 0.5;
-//        double N = 256;
-
-        double power = 0.5;
-        double N = 0;
 
         double x = (65535 + N) / pow (65535, power);
         l = pow(l, power) * x - N;
@@ -81,6 +62,23 @@ int main()
         if (i % 8 == 7)
             printf("\n");
     }
+    printf("\n");
+}
+
+int main()
+{
+    print_sinus_table();
+
+//    print_hamming_table(0.5, 0.5);    // Hann
+    print_hamming_table(0.53836, 0.46164);  // Hamming
+
+//    print_db_table(1.0, 0);
+//    print_db_table(0.9, 2);
+//    print_db_table(0.8, 8);
+//    print_db_table(0.7, 27);
+//    print_db_table(0.6, 84);
+//    print_db_table(0.5, 256);
+    print_db_table(0.5, 0);
 
     return 0;
 }
